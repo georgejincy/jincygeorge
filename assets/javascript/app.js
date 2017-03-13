e0d18dbd7847cd4a0227fc19c13d9b5e38643d2d
georgejincy <jincydon101@gmail.com>
2017-03-13 14:14:23 -0400

    Multiple changes to layout, portfolio, skills

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+
+
+  //Change background color of navbar when scrolling
+  if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#F99471');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
+  }
+
+  //Trigger tooltips for social icons
+  $('[data-toggle="tooltip"]').tooltip({placement : 'bottom'});   
+
+});
